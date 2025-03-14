edaea660482e8d22373f1734fbbed91dc5225fce
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-15 00:06:47 +0800

    fix version

--- packages/wasm/prepare.sh
+++ packages/wasm/prepare.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DATE=$(date +%Y%m%d)
-sed -i '' "s/DATE/$DATE/g" package.json
+# Only works on Linux
+sed -i "s/DATE/$DATE/g" package.json
 
 cp ../../dist/tsgo-wasm_js_wasm/tsgo.wasm .
